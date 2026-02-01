e46721f4a17e00badc963f2ccb0305c28f38549b
Sergii Vdovareize <servdo@gmail.com>
2026-02-01 18:24:52 +0200

    Add CommonJS entrypoint and exports field for CJS/ESM compatibility

---
+++ src/index.cjs
@@ -0,0 +1,10 @@
+const snapsavePkg = require('snapsave-media-downloader');
+const save = snapsavePkg.snapsave || (snapsavePkg.default && snapsavePkg.default.snapsave) || snapsavePkg.default || snapsavePkg;
+
+function snapsave(url) {
+  return save(url);
+}
+
+module.exports = snapsave;
+module.exports.snapsave = snapsave;
+module.exports.default = snapsave;
